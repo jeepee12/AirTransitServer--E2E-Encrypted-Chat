bdb1b824954238b03fa528ddd14c9d935adace82
Jonathan Martineau <jo_martineau_7@hotmail.com>
2018-02-01 15:32:12 -0500

    Added Swagger

--- AirTransitServer/Startup.cs
+++ AirTransitServer/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace AirTransitServer
 {
@@ -23,6 +24,12 @@ namespace AirTransitServer
             services.AddDbContext<RegistryContext>(opt => opt.UseInMemoryDatabase("MyDB"));
             //services.AddDbContext<RegistryContext>(opt => opt.UseSqlite("MyDB"));
             services.AddMvc();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +39,15 @@ namespace AirTransitServer
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
         }
