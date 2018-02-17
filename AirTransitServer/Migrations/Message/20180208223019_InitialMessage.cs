39f0115f3c7646132902432603d031eb645c1901
Jonathan Martineau <jo_martineau_7@hotmail.com>
2018-02-16 21:28:14 -0500

    Added initial migrations

---
+++ AirTransitServer/Migrations/Message/20180208223019_InitialMessage.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AirTransitServer.Migrations.Message
+{
+    public partial class InitialMessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Content = table.Column<string>(nullable: false),
+                    PhoneNumber = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
---
+++ AirTransitServer/Migrations/Registry/20180208223001_InitialRegistry.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AirTransitServer.Migrations
+{
+    public partial class InitialRegistry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Registries",
+                columns: table => new
+                {
+                    PhoneNumber = table.Column<string>(nullable: false),
+                    PublicKey = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registries", x => x.PhoneNumber);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registries");
+        }
+    }
+}
